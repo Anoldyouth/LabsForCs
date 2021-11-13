0dfd2d55a864f8bed2be70bea7ebace4a2d9d7e1
Anoldyouth <83974142+Anoldyouth@users.noreply.github.com>
2021-11-13 22:54:28 +0300

    Add files via upload

---
+++ Lab4c#/IRateAndCopy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab4cs
+{
+    public interface IRateAndCopy
+    {
+        double Rating { get; }
+        object DeepCopy();
+    }
+}
---
+++ Lab4c#/Update.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab4cs
+{
+    enum Update { Add, Replace, Property}
+}
